0d62e91f65dba3ef7b7ca2c60028bf0b9a3bd1ec
Matthew Kim <38759997+friendlymatthew@users.noreply.github.com>
2024-09-26 12:03:48 -0400

    Print spatial layers upon encode

--- js/worker.js
+++ js/worker.js
@@ -40,28 +40,13 @@ function encodeFunction(encodedFrame, controller) {
     if (scount++ < 30) { // dump the first 30 packets.
         dump(encodedFrame, 'send');
     }
-    if (currentCryptoKey) {
-        const view = new DataView(encodedFrame.data);
-        // Any length that is needed can be used for the new buffer.
-        const newData = new ArrayBuffer(encodedFrame.data.byteLength + 5);
-        const newView = new DataView(newData);
 
-        const cryptoOffset = useCryptoOffset ? frameTypeToCryptoOffset[encodedFrame.type] : 0;
-        for (let i = 0; i < cryptoOffset && i < encodedFrame.data.byteLength; ++i) {
-            newView.setInt8(i, view.getInt8(i));
-        }
-        // This is a bitwise xor of the key with the payload. This is not strong encryption, just a demo.
-        for (let i = cryptoOffset; i < encodedFrame.data.byteLength; ++i) {
-            const keyByte = currentCryptoKey.charCodeAt(i % currentCryptoKey.length);
-            newView.setInt8(i, view.getInt8(i) ^ keyByte);
-        }
-        // Append keyIdentifier.
-        newView.setUint8(encodedFrame.data.byteLength, currentKeyIdentifier % 0xff);
-        // Append checksum
-        newView.setUint32(encodedFrame.data.byteLength + 1, 0xDEADBEEF);
+    const metadata = encodedFrame.getMetadata();
+    console.log(
+        "frame to encode==\n\t", performance.now().toFixed(2),
+        "\n\tTemporal Index", metadata.temporalIndex,
+        "\n\t Spatial Index:", metadata.spatialIndex)
 
-        encodedFrame.data = newData;
-    }
     controller.enqueue(encodedFrame);
 }
 
@@ -72,11 +57,11 @@ function decodeFunction(encodedFrame, controller) {
         dump(encodedFrame, 'recv');
     }
 
-    const metadata = encodedFrame.getMetadata();
-    console.log(
-        "frame==\n\t", performance.now().toFixed(2),
-        "\n\tTemporal Index", metadata.temporalIndex,
-        "\n\t Spatial Index:", metadata.spatialIndex)
+    // const metadata = encodedFrame.getMetadata();
+    // console.log(
+    //     "frame==\n\t", performance.now().toFixed(2),
+    //     "\n\tTemporal Index", metadata.temporalIndex,
+    //     "\n\t Spatial Index:", metadata.spatialIndex)
 
     const view = new DataView(encodedFrame.data);
     console.log("encoded frame data: ", view.byteLength);
@@ -119,8 +104,6 @@ function handleTransform(operation, readable, writable) {
         readable
             .pipeThrough(transformStream)
             .pipeTo(writable);
-
-
     } else if (operation === 'decode') {
         const transformStream = new TransformStream({
             transform: decodeFunction,
