4592f6819f9424df6386ebe18e16691c95bf4e42
Matthew Kim <38759997+friendlymatthew@users.noreply.github.com>
2024-10-07 10:00:57 -0400

    Print frame size, check duplicate timestamps exist

--- js/worker.js
+++ js/worker.js
@@ -8,11 +8,34 @@
 // collect all timestmaps -- see # of timestamps  -> frame
 // print the frame size
 
+let timestampCatalog = new Map();
+
 const videoDecoder = new VideoDecoder({
     output: (frame) => {
-        console.log("Good frame", frame.timestamp, frame);
-        postMessage({operation: "videoframe", frame}, [frame]);
+        let {
+            codedHeight,
+            codedWidth,
+            colorSpace,
+            displayHeight,
+            displayWidth,
+            duration,
+            format,
+            timestamp,
+            visibleRect
+        } = frame;
 
+        console.log("Good frame", {timestamp, codedWidth, codedHeight});
+
+
+        if (timestampCatalog.has(timestamp)) {
+            timestampCatalog.set(timestamp, timestampCatalog.get(timestamp) + 1);
+            console.log(`Duplicate timestamp video frame encountered`);
+        } else {
+            timestampCatalog.set(timestamp, 1);
+        }
+
+
+        postMessage({operation: "videoframe", frame}, [frame]);
     },
     error: (error) => {
         let message = error.message;
@@ -36,10 +59,10 @@ async function initializeDecoder() {
             console.log("Video Decoder configuration is supported:", support.config);
             videoDecoder.configure(support.config);
         } else {
-            console.error("Configuration is not supported");
+            return new Error("Configuration is not supported");
         }
     } catch (e) {
-        throw new Error("Something went wrong when checking if isConfigSupported")
+        throw new Error(`Decoder init error ${e}`)
     }
 }
 
@@ -61,7 +84,7 @@ async function handleTransform(operation, readable, writable) {
                 controller.enqueue(encodedFrame);
 
                 if (temporalIndex < highestTemporalLayer && spatialIndex < highestSpatialLayer) {
-                    console.log("Decoding: ", {temporalIndex, spatialIndex})
+                    console.log("Decoding encodedChunk: ", {timestamp, temporalIndex, spatialIndex})
 
                     const chunk = new EncodedVideoChunk({
                         timestamp,
