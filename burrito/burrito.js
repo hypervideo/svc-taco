4aa3cb6efe5cd819d84db6c5bf494063e49d238f
Matthew Kim <38759997+friendlymatthew@users.noreply.github.com>
2024-10-24 10:41:00 -0400

    Simulcast

---
+++ burrito/burrito.js
@@ -0,0 +1,236 @@
+'use strict';
+
+// non negotiable constants
+const av1Profile = 'video/av01.0.04M.08';
+const scalabilityMode = 'L1T3';
+
+const spatial240pVideoElement = document.getElementById('spatial-240-video');
+const spatial480pVideoElement = document.getElementById('spatial-480-video');
+const spatial720pVideoElement = document.getElementById('spatial-720-video');
+
+const spatial240pRemoteVideoElement = document.getElementById('spatial-240-remote-video');
+const spatial480pRemoteVideoElement = document.getElementById('spatial-480-remote-video');
+const spatial720pRemoteVideoElement = document.getElementById('spatial-720-remote-video');
+
+const spatial240pWebCodecVideoElement = document.getElementById('spatial-240-webcodec-video');
+const spatial480pWebCodecVideoElement = document.getElementById('spatial-480-webcodec-video');
+const spatial720pWebCodecVideoElement = document.getElementById('spatial-720-webcodec-video');
+
+const mediaStreamTrackGenerator240p = new MediaStreamTrackGenerator({kind: 'video'});
+const mediaStreamWritable240p = mediaStreamTrackGenerator240p.writable;
+
+const mediaStreamTrackGenerator480p = new MediaStreamTrackGenerator({kind: 'video'});
+const mediaStreamWritable480p = mediaStreamTrackGenerator480p.writable;
+
+const mediaStreamTrackGenerator720p = new MediaStreamTrackGenerator({kind: 'video'});
+const mediaStreamWritable720p = mediaStreamTrackGenerator720p.writable;
+
+
+let resolutions = [
+    {
+        title: "240p",
+        width: 320,
+        height: 240,
+        videoElement: spatial240pVideoElement,
+        stream: null,
+        videoPipe: null,
+        remoteStream: null,
+        remoteVideoElement: spatial240pRemoteVideoElement,
+        webCodecVideoElement: spatial240pWebCodecVideoElement,
+        webCodecTrackGenerator: mediaStreamTrackGenerator240p,
+        webCodecWritable: mediaStreamWritable240p,
+    },
+    {
+        title: "480p",
+        width: 640,
+        height: 480,
+        videoElement: spatial480pVideoElement,
+        stream: null,
+        videoPipe: null,
+        remoteStream: null,
+        remoteVideoElement: spatial480pRemoteVideoElement,
+        webCodecVideoElement: spatial480pWebCodecVideoElement,
+        webCodecTrackGenerator: mediaStreamTrackGenerator480p,
+        webCodecWritable: mediaStreamWritable480p,
+    },
+    {
+        title: "720p",
+        width: 1280,
+        height: 720,
+        videoElement: spatial720pVideoElement,
+        stream: null,
+        videoPipe: null,
+        remoteStream: null,
+        remoteVideoElement: spatial720pRemoteVideoElement,
+        webCodecVideoElement: spatial720pWebCodecVideoElement,
+        webCodecTrackGenerator: mediaStreamTrackGenerator720p,
+        webCodecWritable: mediaStreamWritable720p,
+    },
+];
+
+const worker = new Worker('./worker.js', {name: 'E2EE worker', type: 'module'});
+
+for (let idx = 0; idx <= resolutions.length - 1; idx++) {
+    const {webCodecWritable: writable, title} = resolutions[idx];
+
+    worker.postMessage({
+        operation: `init-${title}`,
+        writable
+    }, [writable])
+}
+
+worker.onmessage = async ({data}) => {
+    const {operation} = data;
+
+    if (operation.startsWith('track-ready')) {
+        const resolutionMessage = operation.split('-')[2];
+
+        let idx;
+        switch (resolutionMessage) {
+            case '240p':
+                idx = 0;
+                break;
+
+            case '480p':
+                idx = 1;
+                break;
+
+            case '720p':
+                idx = 2;
+                break;
+
+            default:
+                throw new Error(`Unsupported resolution: ${resolutionMessage}`);
+        }
+
+        const {webCodecVideoElement, webCodecTrackGenerator} = resolutions[idx];
+        webCodecVideoElement.srcObject = new MediaStream([webCodecTrackGenerator]);
+    }
+
+}
+
+document.addEventListener('keydown', (e) => {
+    switch (e.key) {
+        case '1': {
+            e.preventDefault();
+            !startButton.disabled && start();
+            break;
+        }
+        case '2': {
+            e.preventDefault();
+            !callButton.disabled && call();
+            break;
+        }
+        case '3': {
+            e.preventDefault();
+            !hangupButton.disabled && hangup();
+            break;
+        }
+        default:
+            break;
+    }
+});
+
+const startButton = document.getElementById('startButton');
+const callButton = document.getElementById('callButton');
+const hangupButton = document.getElementById('hangupButton');
+
+startButton.onclick = start;
+callButton.onclick = call;
+hangupButton.onclick = hangup;
+
+async function requestLocalStream(resolution) {
+    let {width: videoWidth, height: videoHeight, videoElement, title} = resolution;
+
+    const options = {
+        audio: false,
+        video: {videoWidth, videoHeight}
+    };
+
+    let stream = await navigator.mediaDevices.getUserMedia(options);
+    console.log("Get user media: ", title, {videoWidth, videoHeight});
+    videoElement.srcObject = stream;
+    resolution.stream = stream;
+
+    try {
+        const {supported, smooth, powerEfficient} = await navigator.mediaCapabilities.encodingInfo({
+            type: 'webrtc',
+            video: {
+                contentType: av1Profile,
+                width: videoWidth,
+                height: videoHeight,
+                bitrate: 10000,
+                framerate: 29.97,
+                scalabilityMode,
+            }
+        });
+    } catch (e) {
+        throw new Error(`Failed to configure WebRTC: ${e}`);
+    }
+}
+
+async function start() {
+    startButton.disabled = true;
+
+    for (let idx = 0; idx <= resolutions.length - 1; idx++) {
+        const resolution = resolutions[idx];
+        await requestLocalStream(resolution);
+    }
+
+    callButton.disabled = false;
+}
+
+async function call() {
+    callButton.disabled = true;
+    hangupButton.disabled = false;
+
+    for (let idx = 0; idx <= resolutions.length - 1; idx++) {
+        let resolution = resolutions[idx];
+
+        console.log(`localStream`, resolution.stream);
+
+        resolution.videoPipe = new VideoPipe(
+            resolution.stream,
+            true,
+            true,
+            (e) => {
+                const receiverStreams = e.receiver.createEncodedStreams();
+                const {readable, writable} = receiverStreams;
+                worker.postMessage(
+                    {
+                        operation: `decode-${resolution.title}`,
+                        readable,
+                        writable,
+                    }, [readable, writable]);
+
+                console.log(`e.streams[0]`, e.streams[0]);
+
+                resolution.remoteVideoElement.srcObject = e.streams[0];
+            },
+            scalabilityMode
+        );
+
+        resolution.videoPipe.pc1.getSenders().forEach((s) => {
+            const senderStreams = s.createEncodedStreams();
+            const {readable, writable} = senderStreams;
+
+            worker.postMessage({
+                operation: `encode-${resolution.title}`,
+                readable,
+                writable
+            }, [readable, writable])
+        });
+
+        await resolution.videoPipe.negotiate();
+    }
+}
+
+async function hangup() {
+    for (let idx = 0; idx <= resolutions.length - 1; idx++) {
+        let resolution = resolutions[idx];
+        resolution.videoPipe.close();
+    }
+
+    hangupButton.disabled = true;
+    callButton.disabled = false;
+}
---
+++ burrito/worker.js
@@ -0,0 +1,136 @@
+'use-strict';
+
+
+let videoDecoder240p, videoDecoder480p, videoDecoder720p;
+
+async function setVideoDecoder(resolution, writer) {
+    console.log(`creating Video Decoder for `, {resolution});
+
+    const videoDecoder = new VideoDecoder({
+        output: async (frame) => {
+            let {
+                codedHeight,
+                codedWidth,
+                colorSpace,
+                displayHeight,
+                displayWidth,
+                duration,
+                format,
+                timestamp,
+                visibleRect,
+            } = frame;
+
+            if (writer) {
+                await writer.write(frame);
+            }
+        },
+        error: (error) => {
+            let message = error.message;
+            let code = error.name;
+
+            throw new Error(`Failed to decode: ${message}, ${code}`);
+        },
+    });
+
+    const config = {
+        codec: 'av01.0.04M.08',
+        hardwareAcceleration: 'prefer-software',
+    };
+
+    try {
+        const support = await VideoDecoder.isConfigSupported(config);
+
+        if (support.supported) {
+            videoDecoder.configure(support.config);
+        } else {
+            return new Error('Configuration is not supported');
+        }
+    } catch (e) {
+        throw new Error(`Decoder init error ${e}`);
+    }
+
+    switch (resolution) {
+        case '240p':
+            videoDecoder240p = videoDecoder;
+            break;
+        case '480p':
+            videoDecoder480p = videoDecoder;
+            break;
+        case '720p':
+            videoDecoder720p = videoDecoder;
+            break;
+        default:
+            throw new Error(`Failed to set video decoder`);
+    }
+}
+
+async function handleTransform(resolution, readable, writable) {
+    const transformer = new TransformStream({
+        async transform(encodedFrame, controller) {
+            const {temporalIndex, spatialIndex, width, height} = encodedFrame.getMetadata();
+            const {timestamp, data, type} = encodedFrame;
+
+            let videoDecoder;
+
+            switch (resolution) {
+                case '240p':
+                    videoDecoder = videoDecoder240p;
+                    break;
+                case '480p':
+                    videoDecoder = videoDecoder480p;
+                    break;
+                case '720p':
+                    videoDecoder = videoDecoder720p;
+                    break;
+                default:
+                    throw new Error(`Unsupported resolution: ${resolution}`);
+            }
+
+            const chunk = new EncodedVideoChunk({
+                timestamp,
+                data,
+                type,
+            });
+
+            await videoDecoder.decode(chunk);
+
+            controller.enqueue(encodedFrame);
+        }
+    })
+
+    await readable.pipeThrough(transformer).pipeTo(writable);
+}
+
+
+// Handler for messages, including transferable streams.
+onmessage = async ({data}) => {
+    let {operation} = data;
+
+    if (operation.startsWith('init')) {
+        const {writable} = data;
+        const resolution = operation.split('-')[1];
+        console.log(`received init`, {resolution, writable});
+
+        const writer = writable.getWriter();
+        await setVideoDecoder(resolution, writer);
+
+        postMessage({
+            operation: `track-ready-${resolution}`,
+        })
+    }
+
+
+    if (operation.startsWith('encode')) {
+        const {readable, writable} = data;
+        await readable.pipeTo(writable);
+    }
+
+    if (operation.startsWith('decode')) {
+        let {readable, writable} = data;
+
+        const resolution = operation.split("-")[1];
+        return await handleTransform(resolution, readable, writable);
+    }
+};
+
+
